740b7530d9e579ce42491668484066a23fa5949f
ahmetrukenboyaci <ahmetrukenboyaci@gmail.com>
2020-09-28 23:52:33 +0300

    FIXED: some styles are fixed #2

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -12,6 +12,9 @@ import internyLogo from "../../assets/interny-logo-white.png";
 import facebook from "../../icons/facebook.svg";
 import twitter from "../../icons/twitter.svg";
 import linkedin from "../../icons/linkedin.svg";
+import appStore from "../../icons/app-store-badge.svg";
+import googlePlay from "../../icons/google-play-badge.svg";
+import location from "../../icons/location-white.svg";
 
 class Footer extends Component {
     render() {
@@ -31,6 +34,15 @@ class Footer extends Component {
                             We are not a recruitment agency.
                             We are an online internship platform that supports the intern and the company from the beginning to the end of the internship process.
                         </div>
+                        <div className={styles.stores}>
+                            <a href={'/'} className={styles.store}><img src={appStore} alt={'store'}/></a>
+                            <a href={'/'} className={styles.store}><img src={googlePlay} alt={'store'}/></a>
+                        </div>
+                        <div className={styles.followUs}>
+                            <a href={'https://www.facebook.com/internynet/'} target={'blank'}><img src={facebook} alt={facebook} /></a>
+                            <a href={'https://twitter.com/internynet/'} target={'blank'}><img src={twitter} alt={twitter} /></a>
+                            <a href={'https://www.linkedin.com/company/internynet/'} target={'blank'}><img src={linkedin} alt={linkedin} /></a>
+                        </div>
                     </div>
                     <div className={styles.options}>
                         <div className={styles.knowUs}>
@@ -49,10 +61,9 @@ class Footer extends Component {
                 <div className={styles.stroke} />
                 <div className={styles.rights}>
                     2020 • INTERNY Inc. © All rights reserved.
-                    <div className={styles.followUs}>
-                        <a href={'https://www.facebook.com/internynet/'} target={'blank'}><img src={facebook} alt={facebook} /></a>
-                        <a href={'https://twitter.com/internynet/'} target={'blank'}><img src={twitter} alt={twitter} /></a>
-                        <a href={'https://www.linkedin.com/company/internynet/'} target={'blank'}><img src={linkedin} alt={linkedin} /></a>
+                    <div className={styles.location}>
+                        <img src={location} alt={'location'} />
+                        <div>{`NL TR UK USA`}</div>
                     </div>
                 </div>
             </div>
