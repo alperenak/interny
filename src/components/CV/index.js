50b9897df4d3a7bcd39e7c4900ceddce0c613ad2
hasandemirkiran <43812957+hasandemirkiran@users.noreply.github.com>
2020-10-27 11:54:15 +0300

    CV accodion design added

--- src/components/CV/index.js
+++ src/components/CV/index.js
@@ -13,19 +13,55 @@ import Languages from "./sub-components/Languages";
 /*** Styles ***/
 import styles from './cv.scss';
 
+//accordion library
+import {
+    Accordion,
+    AccordionItem,
+    AccordionItemHeading,
+    AccordionItemButton,
+    AccordionItemPanel,
+} from 'react-accessible-accordion';
+
 class CV extends Component {
     render() {
         let {file, getCVs} = this.props;
         return (
+            // <div className={styles.CV}>
+            //     <ProfileSection file={file} getCVs={getCVs} />
+            //     <Experiences file={file} getCVs={getCVs} />
+            //     <Education file={file} getCVs={getCVs} />
+            //     <Skills file={file} getCVs={getCVs} />
+            //     <Languages file={file} getCVs={getCVs} />
+            //     <Certificates file={file} getCVs={getCVs} />
+            //     <AdditionalInfo file={file} getCVs={getCVs} />
+            // </div>
+
             <div className={styles.CV}>
-                <ProfileSection file={file} getCVs={getCVs} />
-                {/* <Experiences file={file} getCVs={getCVs} />
-                <Education file={file} getCVs={getCVs} />
-                <Skills file={file} getCVs={getCVs} />
-                <Languages file={file} getCVs={getCVs} />
-                <Certificates file={file} getCVs={getCVs} />
-                <AdditionalInfo file={file} getCVs={getCVs} /> */}
+                <Accordion>
+                <AccordionItem>
+                    <AccordionItemHeading>
+                        <AccordionItemButton>
+
+                             <ProfileSection file={file} getCVs={getCVs} />
+                             
+                        </AccordionItemButton>
+                    </AccordionItemHeading>
+                    <AccordionItemPanel>
+
+                    <Experiences file={file} getCVs={getCVs} />
+                    <Education file={file} getCVs={getCVs} />
+                    <Skills file={file} getCVs={getCVs} />
+                    <Languages file={file} getCVs={getCVs} />
+                    <Certificates file={file} getCVs={getCVs} />
+                    <AdditionalInfo file={file} getCVs={getCVs} />
+
+                    </AccordionItemPanel>
+                </AccordionItem>
+            </Accordion>
             </div>
+                
+
+
         );
     }
 }
