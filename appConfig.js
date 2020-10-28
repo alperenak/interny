79e87e0a16f48ce6119f65f448afec18f7db8fe6
Abdulkader El Rawas <63848905+abdulkaderelrawas@users.noreply.github.com>
2020-10-28 15:34:05 +0300

    branch initialization, project config modification to work.

--- appConfig.js
+++ appConfig.js
@@ -1,5 +1,5 @@
 const config = {
-    baseUrl: process.env.NODE_ENV === 'development' ? 'https://interny-backend-prod.herokuapp.com/' : ''
+  baseUrl: 'https://interny-backend-prod.herokuapp.com/',
 };
 
 module.exports = config;
