a37f39598aeb094d99f1d1476603384a66c02d85
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2020-12-14 16:08:32 +0300

    Investor page: Update content and use HighlightBox

--- src/screens/internyPages/investor.js
+++ src/screens/internyPages/investor.js
@@ -1,197 +1,224 @@
-import React, { Component } from "react";
-import ReactDOM, { Link } from "react-dom";
-import Input from "../../components/Input";
-import Button from "../../components/Button";
-import Card from "../../components/Card";
-import FooterAlternative from "../../components/FooterAlternative";
-import styles from "./style.scss";
-import handshake from "../../assets/handshake.jpg";
-import worldwide from "../../assets/worldwide.png";
-import review_applicant from "../../assets/review-applicant.png";
-import start_internship from "../../assets/start-internship.png";
-import manage_program from "../../assets/manage-program.png";
-import investorBg from "../../assets/investorBg.png";
+import React, { Component } from 'react';
+import './style.scss';
+
+// Components
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import Footer from '../../components/Footer';
+import HighlightBox from '../../components/HighlightBox';
+
+// Assets
+import investorBg from '../../assets/investorBg.png';
+import wfaReferenceLetter from '../../icons/wfa-reference-letter.png';
+
+const HIGHLIGHT_BOXES = [
+	{
+		title: 'SaaS',
+		icon: wfaReferenceLetter,
+	},
+	{
+		title: 'The First Platform',
+		icon: wfaReferenceLetter,
+	},
+	{
+		title: 'Worldwide',
+		icon: wfaReferenceLetter,
+	},
+	{
+		title: 'Generation-Z',
+		icon: wfaReferenceLetter,
+	},
+	{
+		title: 'East-West Bridge',
+		icon: wfaReferenceLetter,
+	},
+	{
+		title: 'Artificial Intelligence',
+		icon: wfaReferenceLetter,
+	},
+];
+
 class InvestorPage extends Component {
-	state = {
-		pri1: false,
-		pri2: false,
-		pri3: false,
+	renderBoxes = () => {
+		return HIGHLIGHT_BOXES.map((element, index) => {
+			return (
+				<div class="col-md-4">
+					<HighlightBox icon={element.icon} title={element.title} />
+				</div>
+			);
+		});
 	};
-	componentDidMount(){
 
-	}
-    render() {
-        return (
+	render() {
+		return (
 			<>
-			<div class="internyPage">
-				<div class="affiliate__header">
-					<div class="container" style={{"background-image":"url("+investorBg+")"}}>
-						<p>INVESTOR</p>
-					</div>
-				</div>
-				<div class="internyPage__twoBox">
-					<div class="container">
-						<div class="row">
-							<div class="col-md-12">
-								<div class="internyPage__box">
-									<span>Company Profile</span>
-									<p>
-										Opsens focuses mainly on the measure of FFR in interventional cardiology. Opsens offers an advanced optical-based pressure guidewire (OptoWire) that aims at improving the clinical outcome of patients with coronary artery disease. Opsens is also involved in industrial activities in developing, manufacturing and installing innovative fibre optic sensing solutions for critical applications.
-									</p>
-								</div>
-							</div>
-
+				<div class="internyPage">
+					<div class="affiliate__header">
+						<div
+							class="container"
+							style={{ 'background-image': 'url(' + investorBg + ')' }}
+						>
+							<p>INVESTOR</p>
 						</div>
 					</div>
-				</div>
-				<div class="internyPage__twoBox">
-					<div class="container">
-						<div class="row">
-							<div class="col-md-6">
-								<div class="internyPage__box">
-									<span>Mission Statement</span>
-									<p>
-										Interny will produce superior financial returns for its shareowners by providing high value-added logistics, transportation and related business services through focused operating companies. Customer requirements will be met in the highest quality manner appropriate to each market segment served. FedEx will strive to develop mutually rewarding relationships with its team members, partners and suppliers. Safety will be the first consideration in all operations. Corporate activities will be conducted to the highest ethical and professional standards.
-									</p>
-								</div>
-							</div>
-							<div class="col-md-6">
-								<div class="internyPage__box">
-									<span>Why Invest</span>
-									<p>
-										Interny will produce superior financial returns for its shareowners by providing high value-added logistics, transportation and related business services through focused operating companies. Customer requirements will be met in the highest quality manner appropriate to each market segment served. FedEx will strive to develop mutually rewarding relationships with its team members, partners and suppliers. Safety will be the first consideration in all operations. Corporate activities will be conducted to the highest ethical and professional standards.
-									</p>
+					<div class="internyPage__twoBox">
+						<div class="container">
+							<div class="row">
+								<div class="col-md-12">
+									<div class="internyPage__box">
+										<span>Company Profile</span>
+										<p>
+											INTERNY Inc. is the world's first remote online internship
+											platform company, headquartered in Silicon Valley,
+											California. It also has offices in Rotterdam, Istanbul,
+											London, and Dubai. INTERNY Inc., which provides a global
+											remote online internship opportunity in the globalized
+											world, offers its investors the chance to high profit.
+                    					</p>
+									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-				<div class="affiliate__triple">
-					<div class="container">
-						<div class="row">
-							<div class="col-md-12" style={{"text-align":"center","margin-bottom":"30px"}}>
-								<span class="affiliate__timeline__title">FEATURES REPORTS</span>
+					<div class="internyPage__twoBox">
+						<div class="container">
+							<div class="row">
+								<div class="col-md-6">
+									<div class="internyPage__box">
+										<span>Mission Statement</span>
+										<p>
+											INTERNY aims to bring together interns and companies
+											around the world and offers a remote online internship
+											adventure. In this way, it brings together companies in
+											developed countries and interns in developing countries,
+											eliminates the borders between countries, and creates
+											equal opportunities. Also, it offers a brand new
+											internship experience for both companies, interns, and
+											universities with its platforms that appeal to the more
+											digitized Z generation.
+                    					</p>
+									</div>
+								</div>
+								<div class="col-md-6">
+									<div class="internyPage__box">
+										<span>Why Invest</span>
+										<p>
+											INTERNY Inc. offers the opportunity to find each other for
+											all interns and companies and allows internships to be
+											managed through the platform from beginning to the end of
+											the internship. It is the first company in the world to
+											have a remote online internship platform. INTERNY Inc.
+											carries out its operations alone in a vast market.
+											Moreover, it is a company that is open to growth and
+											offers its investors the opportunity to profit with its
+											team of expert and fully open mind people.
+                    					</p>
+									</div>
+								</div>
 							</div>
 						</div>
-						<div class="row">
-							<div class="col-md-4">
-								<div class="affiliate__tripleBox">
-									<span class="affiliate__tripleBox__title">
-										Lorem ipsum dolor sit amet
-									</span>
-									<span class="affiliate__tripleBox__description">
-										Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-									</span>
+					</div>
+					<div class="affiliate__triple">
+						<div class="container">
+							<div class="row">
+								<div
+									class="col-md-12"
+									style={{ 'text-align': 'center', 'margin-bottom': '30px' }}
+								>
+									<span class="affiliate__timeline__title">Why Interny?</span>
 								</div>
 							</div>
-							<div class="col-md-4">
-								<div class="affiliate__tripleBox">
-									<span class="affiliate__tripleBox__title">
-										Lorem ipsum dolor sit amet
-									</span>
-									<span class="affiliate__tripleBox__description">
-										Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-									</span>
+							<div class="row">{this.renderBoxes()}</div>
+						</div>
+					</div>
+					<div className="affiliate__contact">
+						<div class="container">
+							<div class="row">
+								<div
+									class="col-md-12"
+									style={{ 'text-align': 'center', 'margin-bottom': '30px' }}
+								>
+									<span class="affiliate__timeline__title">
+										You can contact us with any questions.
+                  					</span>
 								</div>
 							</div>
-							<div class="col-md-4">
-								<div class="affiliate__tripleBox">
-									<span class="affiliate__tripleBox__title">
-										Lorem ipsum dolor sit amet
-									</span>
-									<span class="affiliate__tripleBox__description">
-										Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-									</span>
+							<div class="row">
+								<div class="col-md-6">
+									<Input
+										type={'text'}
+										placeholder={'Name'}
+										size={'full'}
+										onChange={(value) => {
+											this.setState({ city: value });
+										}}
+										label={'Name'}
+									/>
+								</div>
+								<div class="col-md-6">
+									<Input
+										type={'text'}
+										placeholder={'Surname'}
+										size={'full'}
+										onChange={(value) => {
+											this.setState({ city: value });
+										}}
+										label={'Surname'}
+									/>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-md-6">
+									<Input
+										type={'text'}
+										placeholder={'E-mail'}
+										size={'full'}
+										onChange={(value) => {
+											this.setState({ city: value });
+										}}
+										label={'E-mail'}
+									/>
+								</div>
+								<div class="col-md-6">
+									<Input
+										type={'text'}
+										placeholder={'Phone'}
+										size={'full'}
+										onChange={(value) => {
+											this.setState({ city: value });
+										}}
+										label={'Phone'}
+									/>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-md-12">
+									<Input
+										type={'textarea'}
+										placeholder={'Message'}
+										size={'full'}
+										onChange={(value) => {
+											this.setState({ city: value });
+										}}
+										label={'Message'}
+									/>
+								</div>
+							</div>
+							<div class="row">
+								<div
+									class="col-md-12"
+									style={{ display: 'flex', justifyContent: 'flex-end' }}
+								>
+									<Button type={'primary'} text={'Send'} sizeName={'default'} />
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-				<div className="affiliate__contact">
-					<div class="container">
-						<div class="row">
-							<div class="col-md-6">
-								<Input
-									type={"text"}
-									placeholder={"Name"}
-									size={"full"}
-
-									onChange={(value) => {
-									this.setState({ city: value });
-									}}
-									label={"Name"}
-								/>
-							</div>
-							<div class="col-md-6">
-								<Input
-									type={"text"}
-									placeholder={"Surname"}
-									size={"full"}
-
-									onChange={(value) => {
-									this.setState({ city: value });
-									}}
-									label={"Surname"}
-								/>
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-md-6">
-								<Input
-									type={"text"}
-									placeholder={"E-mail"}
-									size={"full"}
-
-									onChange={(value) => {
-									this.setState({ city: value });
-									}}
-									label={"E-mail"}
-								/>
-							</div>
-							<div class="col-md-6">
-								<Input
-									type={"text"}
-									placeholder={"Phone"}
-									size={"full"}
-
-									onChange={(value) => {
-									this.setState({ city: value });
-									}}
-									label={"Phone"}
-								/>
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-md-12">
-								<Input
-									type={"textarea"}
-									placeholder={"Message"}
-									size={"full"}
-
-									onChange={(value) => {
-									this.setState({ city: value });
-									}}
-									label={"Message"}
-								/>
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-md-12">
-								<Button
-									type={"primary"}
-									text={"Send"}
-									sizeName={"default"}
-								/>
-
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<FooterAlternative />
+				<Footer />
 			</>
-        );
-    }
+		);
+	}
 }
 
 export default InvestorPage;
